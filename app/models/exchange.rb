ad5051a7d64b7197f196dfb132bd7590311b274f
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-17 18:39:46 +0300

    Create currencies_options

--- app/models/exchange.rb
+++ app/models/exchange.rb
@@ -1,26 +1,6 @@
 class Exchange < ApplicationRecord
-    after_create :save!
-
-    def save!
-        create_or_update || raise(RecordNotSaved)
-    end
-
-    # def create_or_update
-    #     self.currency_data = currency_data()
-    #     super
-    # end
-
-    # def currency_data
-    #     # url = URI("https://api.apilayer.com/fixer/latest")
-    #     url = URI("https://api.apilayer.com/currency_data/list")
-
-    #     https = Net::HTTP.new(url.host, url.port);
-    #     https.use_ssl = true
-        
-    #     request = Net::HTTP::Get.new(url)
-    #     request['apikey'] = "9aMBOMPD3Az3URY0ZXnFnF8AH3oYQWPm"
-        
-    #     response = https.request(request)
-    #     return response.read_body
-    # end
+  currencies_options = %w[usd eur gbp aud cad chf cny dkk hkd inr jpy mxn nok nzd pln ron sek sgd thb zar]
+  validates :amount, presence: true
+  validates :amount_to_calculate, presence: true
+  validates :currency, presence: true, inclusion: { in: currencies_options }
 end
