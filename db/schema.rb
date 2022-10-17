ad3347d36d8ae185de2a1633ec5ccd62315ced5d
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-17 18:40:42 +0300

    Migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_15_131941) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_153240) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "exchanges", force: :cascade do |t|
-    t.string "currency_data"
+    t.integer "amount"
+    t.string "currrency"
+    t.integer "amount_to_calculate"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
