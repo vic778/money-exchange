50b7895805caa154e1272dac616903f90d0cc8c3
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-18 12:52:46 +0300

    Make inde as root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'exchanges/index'
+  # get 'exchanges/index'
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "exchanges#index"
   resources :exchanges
+
 end
