4c62b1631a64a59389654821ebe81ff6c581b2d9
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-18 23:50:24 +0300

    Create & Migrate currrency model

--- db/migrate/20221018121223_create_currencies.rb
+++ db/migrate/20221018121223_create_currencies.rb
@@ -2,6 +2,7 @@ class CreateCurrencies < ActiveRecord::Migration[7.0]
   def change
     create_table :currencies do |t|
       t.string :name
+      t.float :currency
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_18_121223) do
 
   create_table "currencies", force: :cascade do |t|
     t.string "name"
+    t.float "currency"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
