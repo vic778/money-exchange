9e0e5b93f699aa1c96e9eaf0394cb4ca5e6cc0c2
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-19 18:05:59 +0300

    Remove update_currency method

--- app/controllers/exchanges_controller.rb
+++ app/controllers/exchanges_controller.rb
@@ -33,22 +33,5 @@ class ExchangesController < ApplicationController
     end
   end
 
-  def update_currency
-    url = URI("https://api.apilayer.com/fixer/latest")
-
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
-
-    request = Net::HTTP::Get.new(url)
-    request['apikey'] = "6n6ZUfT4WVoRBN8EbAk4kSnplNskUOlR"
-
-    response = https.request(request)
-    res = JSON.parse(response.read_body)
-    res["rates"].each do |key, value|
-      name = key.gsub(/---/, '').gsub(/\n/, '')
-      currency = value
-      @currency = Currency.new(name: name, currency: currency)
-      @currency.save
-    end
-  end
+  def update_currency; end
 end
