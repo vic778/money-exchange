5f16563d092e6f8791fe55dda83db6aa695601a8
vic778 <victoiremmanuelbarh@gmail.com>
2022-10-19 14:16:54 +0300

    Create from and to constatbce

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -2,4 +2,8 @@ class Currency < ApplicationRecord
   # serialize :name
   FROM = %w[USD EUR GBP AUD CAD EAD UGX].freeze
   TO = %w[USD EUR GBP AUD CAD EAD UGX].freeze
+
+  # validates :amount, presence: true
+  # validates :from, presence: true, inclusion: { in: FROM }
+  # validates :to, presence: true, inclusion: { in: TO }
 end
